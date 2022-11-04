61ee9067d80d5196c3228b205bb13fce1f6c5db2
rachelho <rachho1998@gmail.com>
2022-11-04 16:38:29 -0700

    qa storing question info as expected now, also temporary story structure

--- qa.py
+++ qa.py
@@ -7,17 +7,18 @@ from nltk import pos_tag
 from nltk.stem import WordNetLemmatizer
 def main():
     responseFile = 'responseFile.csv'
-    stories = []
+    stories = {}
     #each questions key is the id, followed by the type of question, then the question itself
     questions = {}
     ids = []
     stop = stopwords.words('english')
+    path = ''
     with open(sys.argv[1]) as input:
         lines = input.readlines()
-        path = lines[0]
+        path = lines[0].strip()
         for qids in lines[1:]:
             qid = qids.strip()
-            ids.append(qid)
+            ids.append(path + qid)
     for id in ids:
         questionFile = id + ".questions"
         storyFile = id + ".story"
@@ -25,61 +26,155 @@ def main():
         with open(questionFile) as qfile:
             lines = qfile.readlines()
             questionCount = 0
-            questions[id] = []
-            for line in lines():
+            questions[id] = {}
+            for line in lines:
+                if(line == "\n"):
+                    break
                 l = line.split()
                 tag = l[0]
-                item = l[1:]
+                # if(tag == "QuestionID:"):
+                #     questions[l[1].strip()] = {}
                 # if(tag == "QuestionID:"):
                 #     questionId = str(item)
                 if (tag== "Question:"):
-                    q = ""
+                    item = l[1:]
+                    question = ""
                     #convert to string for token; not sure if necessary
                     for w in item:
                         question+= w
-                    token_q = word_tokenize(q)
-                    #remove stop words
-                    for wr in token_q:
-                        if wr in stop:
-                            token_q.pop(wr)
+                        question+= " "
+                    question = question.strip().lower()
+                    token_q = word_tokenize(question)
+                    #remove question mark
+                    if(token_q[len(token_q)-1][0]) == "?":
+                        token_q.pop()
                     #pos tag
                     pos_q = pos_tag(token_q)
+                    rmv = []
+                    for word in pos_q:
+                        if word[0] in stop:
+                            rmv.append(word)
+                    for rm in rmv:
+                        if rm in pos_q:
+                            pos_q.remove(rm)
+                    #lemme uses different tag system 
+                    lemm_tags = []
+                    for pq in pos_q:
+                        if pq[1].startswith('N'):
+                            lemm_tags.append((pq[0], 'n'))
+                        elif pq[1].startswith('V'):
+                            lemm_tags.append((pq[0], 'v'))
+                        elif pq[1].startswith('R'):
+                            lemm_tags.append((pq[0], 'r'))             
+                        elif pq[1].startswith('J'):
+                            lemm_tags.append((pq[0], 'a'))
+                        else:
+                            lemm_tags.append((pq[0], ''))
                     #lemma
                     lemmatizer = WordNetLemmatizer()
                     lemm_q = []
-                    for wrd in pos_q:
-                        lemm_q.append(lemmatizer.lemmatize(wrd[0], pos=wrd[1]))
+                    for wrd in lemm_tags:
+                        if(wrd[1]!= ''):
+                            lemm_q.append(lemmatizer.lemmatize(wrd[0], pos=wrd[1]))
+                        else:
+                            lemm_q.append(wrd[0])
+                    #combine lemmatized with proper pos
+                    pos_lemm = []
+                    for i in range(0, len(lemm_q)):
+                        pos_lemm.append((lemm_q[i], pos_q[i][1]))
                     #that, what, whatever, which, whichever
                     for ps in pos_q:
-                        if ps[0] == 'WDT':
+                        t = ''
+                        if ps[1] == 'WDT':
                             t = "wh-determiner"
+                            pos_q.remove(ps)
                             questions[id][t] = pos_q
                     #that what whatever whatsoever which who whom whomsover
-                        if ps[0] == 'WP':
+                        if ps[1] == 'WP':
                             t = "wh-pronoun"
+                            pos_q.remove(ps)
                             questions[id][t] = pos_q
                     #whose
-                        if ps[0] == 'WP$':
+                        if ps[1] == 'WP$':
                             t = "wh-pronoun-possessive"
+                            pos_q.remove(ps)
                             questions[id][t] = pos_q
                     #how however whence whenever where whereby whereever wherein whereof why
-                        if ps[0] == 'WRB':
+                        if ps[1] == 'WRB':
                             t = "wh-adverb"
+                            pos_q.remove(ps)
                             questions[id][t] = pos_q
-                    
+        #print(questions)
         with open(storyFile) as sfile:
             lines = sfile.readlines()
-            for line in lines():
+            stories[id] = []
+            sentences = []
+            for line in lines:
+                if(line == "\n"):
+                    continue
                 l = line.split()
+                tag = l[0].strip()
+                #begin reading
+                if (tag!= "TEXT:" and tag!= "HEADLINE:" and tag!= "DATE:" and tag!= "STORYID:"):
+                    if(line == "\n"):
+                        continue
+                    sentence = line.strip().lower()
+                    #print("SENTENCE: " + sentence)
+                    token_s = word_tokenize(sentence)
+                    #pos tag
+                    pos_s = pos_tag(token_s)
+                    rmv = []
+                    for word in pos_s:
+                        if word[0] in stop:
+                            rmv.append(word)
+                    for rm in rmv:
+                        if rm in pos_s:
+                            pos_s.remove(rm)
+                    #lemme uses different tag system 
+                    lemm_tags = []
+                    for ps in pos_s:
+                        if ps[1].startswith('N'):
+                            lemm_tags.append((ps[0], 'n'))
+                        elif ps[1].startswith('V'):
+                            lemm_tags.append((ps[0], 'v'))
+                        elif ps[1].startswith('R'):
+                            lemm_tags.append((ps[0], 'r'))             
+                        elif ps[1].startswith('J'):
+                            lemm_tags.append((ps[0], 'a'))
+                        else:
+                            lemm_tags.append((ps[0], ''))
+                    #lemma
+                    lemmatizer = WordNetLemmatizer()
+                    lemm_s = []
+                    for wrd in lemm_tags:
+                        if(wrd[1]!= ''):
+                            lemm_s.append(lemmatizer.lemmatize(wrd[0], pos=wrd[1]))
+                        else:
+                            lemm_s.append(wrd[0])
+                    #combine lemmatized with proper pos
+                    for i in range(0, len(lemm_s)):
+                        sentences.append((lemm_s[i], pos_s[i][1]))
+            sentenceStarts = []
+            sentenceEnds = []
+            currentSentence = []
+            newSentence = True
+            for i in range(0, len(sentences)):
+                if newSentence:
+                    sentenceStarts.append(i)
+                    newSentence = False
+                elif sentences[i][1] == ".":
+                    sentenceEnds.append(i+1)
+                    newSentence = True
+            for i in range(0, len(sentenceStarts)):
+                stories[id].append(sentences[sentenceStarts[i]:sentenceEnds[i]-1])
                 #read through story and look for answers based on the question's parts we parsed
                 #if a word in line contains what we are looking for, put it down as the answer
-        with open(responseFile) as rf:
-            writer = csv.writer(responseFile)
-            for q in range(len(questions)):
-                for a in range(answers):
-                    writer.writerow("QuestionID: " + id + a)
-                    writer.writerow("Answer: " + answers[a])
-        for q in questions[id]:
+        # with open(responseFile) as rf:
+        #     writer = csv.writer(responseFile)
+        #     for q in range(len(questions)):
+        #         for a in range(answers):
+        #             writer.writerow("QuestionID: " + id + a)
+        #             writer.writerow("Answer: " + answers[a])
             
 if __name__ == "__main__":
     main()
